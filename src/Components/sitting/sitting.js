17ab66f262fe6e4cb1c0bd50e6c3f9bf5d0f6506
Calil-Silva <ka.renner@hotmail.com>
2021-09-02 11:27:16 -0300

    requisicao dos assentos concluida

--- src/Components/sitting/sitting.js
+++ src/Components/sitting/sitting.js
@@ -1,13 +1,25 @@
 import './sitting.css'
 import { useParams } from 'react-router-dom'
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 
 export default function Sitting() {
 
     const { idSessao } = useParams();
-    console.log(idSessao)
+    const [seats, setSeats] = useState([]);
 
+    useEffect(() => {
+        const promise = axios.get(`https://mock-api.bootcamp.respondeai.com.br/api/v2/cineflex/showtimes/${idSessao}/seats`)
+        promise.then((response) => {
+            setSeats([response.data])
+        })
+    }, [])
+
+console.log(seats)
 
     return (
-        null
+        <section>
+            <h1>Selecione o(s) assento(s)</h1>
+        </section>
     )
 }
