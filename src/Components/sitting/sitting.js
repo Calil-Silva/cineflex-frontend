f286144cb4be77c9ab1d341cf6f67424de0daeed
Calil-Silva <ka.renner@hotmail.com>
2021-09-02 22:18:47 -0300

    assentos adicionados conforme API

--- src/Components/sitting/sitting.js
+++ src/Components/sitting/sitting.js
@@ -5,13 +5,14 @@ import axios from 'axios';
 
 export default function Sitting() {
 
+    let seatDescription;
     const { idSessao } = useParams();
     const [seats, setSeats] = useState([]);
 
     useEffect(() => {
-        const promise = axios.get(`https://mock-api.bootcamp.respondeai.com.br/api/v2/cineflex/showtimes/${idSessao}/seats`)
+        const promise = axios.get(`https://mock-api.bootcamp.respondeai.com.br/api/v3/cineflex/showtimes/${idSessao}/seats`)
         promise.then((response) => {
-            setSeats([response.data])
+            setSeats([...response.data.seats])
         })
     }, [])
 
@@ -20,13 +21,34 @@ export default function Sitting() {
     return (
         <section>
             <h1>Selecione o(s) assento(s)</h1>
-
             <div className="seats">
-                <div className="seat"></div>
-                <div className="seat"></div>
-                <div className="seat"></div>
+                {seats.map((element, index) => {
+                    if (element.isAvailable) {
+                        seatDescription = "available";
+                    } else {
+                        seatDescription = "unavailable"
+                    }
+                    return (
+                        <div className={`seat ${seatDescription}`} key={index}>{element.name}</div>)
+                    }
+            )
+                }
+            </div>
+            <div className="seats-info">
+                <div className="info">
+                    <div className="seat selected"></div>
+                    <span>Selecionado</span>
+                </div>
 
+                <div className="info">
+                    <div className="seat available"></div>
+                    <span>Disponível</span>
+                </div>
 
+                <div className="info">
+                    <div className="seat unavailable"></div>
+                    <span>Indisponível</span>
+                </div>
             </div>
         </section>
     )
